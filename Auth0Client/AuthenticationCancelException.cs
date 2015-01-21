03ec325e474fc2bd51392808dce9701ede03a21d
Sebastian Iacomuzzi <sebastian.iacomuzzi@gmail.com>
2015-01-21 10:24:58 -0300

    add missing files. fixes #7

---
+++ Auth0Client/AuthenticationCancelException.cs
@@ -0,0 +1,15 @@
+﻿namespace Auth0.SDK
+{
+    using System;
+
+    public class AuthenticationCancelException : Exception
+    {
+        public AuthenticationCancelException()
+        { 
+        }
+
+        public AuthenticationCancelException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Auth0Client/AuthenticationErrorException.cs
@@ -0,0 +1,15 @@
+﻿namespace Auth0.SDK
+{
+    using System;
+
+    public class AuthenticationErrorException : Exception
+    {
+        public AuthenticationErrorException()
+        {
+        }
+
+        public AuthenticationErrorException(string message) : base(message)
+        {
+        }
+    }
+}
